0012805790c3717971336bf9c1eb92e63905d76d
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-02-26 16:05:04 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 namespace SpriteKind {
     export const rock = SpriteKind.create()
+    export const phenom = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.rock, SpriteKind.rock, function (sprite, otherSprite) {
     music.knock.play()
@@ -8,6 +9,11 @@ sprites.onOverlap(SpriteKind.rock, SpriteKind.rock, function (sprite, otherSprit
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Phaser()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.rock, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 500)
+    music.knock.play()
+    info.changeScoreBy(randint(3, 17))
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.rock, function (sprite, otherSprite) {
     sprite.setImage(assets.image`EnterpriseShipShields`)
@@ -22,6 +28,12 @@ function Phaser () {
     zap = sprites.createProjectileFromSprite(assets.image`Phaser`, enterprise, 0, -200)
     zap.setFlag(SpriteFlag.DestroyOnWall, true)
 }
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.phenom, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.rings, 500)
+    music.knock.play()
+    info.changeScoreBy(randint(3, 17))
+})
+let nebula: Sprite = null
 let nurock: Sprite = null
 let zap: Sprite = null
 let enterprise: Sprite = null
@@ -44,5 +56,18 @@ forever(function () {
     nurock = sprites.create(rocks[randint(0, 4)], SpriteKind.rock)
     nurock.setFlag(SpriteFlag.DestroyOnWall, true)
     nurock.setPosition(randint(0, 160), 0)
-    nurock.setVelocity(randint(-50, 50), randint(20, 100))
+    nurock.setVelocity(randint(-50, 50), randint(20, 200))
+})
+forever(function () {
+    pause(randint(500, 5000))
+    nebula = sprites.create(assets.image`neb`, SpriteKind.phenom)
+    animation.runImageAnimation(
+    nebula,
+    assets.animation`myAnim`,
+    200,
+    true
+    )
+    nebula.setPosition(9, randint(0, 10))
+    nebula.setVelocity(randint(-50, 50), 0)
+    nebula.setFlag(SpriteFlag.DestroyOnWall, true)
 })
